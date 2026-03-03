8e56acbe6cc0a8b0a47ebe348b9fda6364937fa7
Prescott Johnson <prescottjohnson@Prescotts-MacBook-Air.local>
2026-03-02 21:26:02 -0700

    added server running on port 8000

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -4,6 +4,9 @@ import chess.*;
 
 public class ServerMain {
     public static void main(String[] args) {
+        Server server = new Server();
+        server.run(8080);
+
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
     }
